fa767dc56d484b62e4497fcb6e33e7a358a232ff
G3ovanny <jeffersonlara98@gmail.com>
2023-12-20 11:49:46 -0500

    Correccion explicacion cronograma

--- app/core/vacaciones/cargarDocumento/cronogramaVacaciones.py
+++ app/core/vacaciones/cargarDocumento/cronogramaVacaciones.py
@@ -17,9 +17,9 @@ def cargarCronograma():
     success_message = []
     error_messages= []
     
-    loes = 'Por lo dispuesto a la ley y en cumplimiento al candelario académico, me permito solicitar de la manera más comedida se autorice mis vacaciones en el periodo indicado. Fecha de reincorporación a actividades'
-    losep = 'Por lo dispuesto a la ley y en cumplimiento al candelario académico, me permito solicitar de la manera más comedida se autorice mis vacaciones en el periodo indicado. Fecha de reincorporación a actividades'
-    codigo = 'Por lo dispuesto a la ley y en cumplimiento al candelario académico, me permito solicitar de la manera más comedida se autorice mis vacaciones en el periodo indicado. Fecha de reincorporación a actividades'
+    loes = 'Por lo dispuesto a la Ley y en cumplimiento al candelario académico, me permito solicitar de la manera más comedida se autorice mis vacaciones en el periodo antes citado. Fecha de reincorporación a actividades'
+    losep = 'Por lo dispuesto a la Ley y en cumplimiento al candelario académico, me permito solicitar de la manera más comedida se autorice mis vacaciones en el periodo antes citado. Fecha de reincorporación a actividades'
+    codigo = 'Por lo dispuesto a la Ley y en cumplimiento al candelario académico, me permito solicitar de la manera más comedida se autorice mis vacaciones en el periodo antes citado. Fecha de reincorporación a actividades'
     
     cedula = 'numero_identificacion' in cronograma.columns
 
